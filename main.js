e54b377a41f8c2ae8f9b946706ca1b853a2f11cf
Karan Vishwakarma <132004206+Karan7385@users.noreply.github.com>
2025-04-21 12:47:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname)));
 
 // Optional: Fallback for SPA routing
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(PORT, () => {
